f8565628d1aa5d554bca395bcefe8bcdee5562cb
Isak Landin <utbisalan@gmail.com>
2026-02-19 09:59:15 +0100

    Fixed reference to correct endpoint, not docmost, docmost-fetcher

--- ui/app_ui.py
+++ ui/app_ui.py
@@ -5,7 +5,7 @@ import requests
 from flask import Flask, jsonify, request, send_from_directory
 
 BACKEND_URL = os.getenv("BACKEND_BASE_URL", "http://backend:8100")
-DOCMOST_FETCHER_BASE_URL = os.getenv("DOCMOST_BASE_URL", "http://docmost:8099")
+DOCMOST_FETCHER_BASE_URL = os.getenv("DOCMOST_FETCHER_BASE_URL", "http://docmost:8099")
 docmost_fetcher_spaces_endpoint = DOCMOST_FETCHER_BASE_URL + os.getenv("SPACES_PATH", "/api/spaces")
 
 UI_LISTEN_HOST = os.getenv("UI_LISTEN_HOST", "0.0.0.0")
